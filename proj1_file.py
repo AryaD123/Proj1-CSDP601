fc95ded9d257a3e72e8e8d20f25f0a5a993f2838
Arya Das <aryad1@umbc.edu>
2024-10-22 17:32:47 -0400

    writing functions for finding GCD, checking if num is prime, and picking random prime nums

--- proj1_file.py
+++ proj1_file.py
@@ -5,3 +5,38 @@ Fall 2024 - UMES
 Project 1
 Description: Program for  implementing RSA Cryptography Algorithm
 """
+
+import random
+import math #using libraries to check if numbers are prime and to select random prime nums
+
+
+# function for greatest common divisor
+def gcd(a, b):
+    temp = 0
+    while(1):
+        temp = a % b
+        if (temp == 0):
+            return b
+        a = b
+        b = temp
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def generate_random_prime(start, end):
+    while True:
+        num = random.randint(start, end)
+        if is_prime(num):
+            return num
+
+if __name__ == '__main__':
+    minPrimeRange = 2
+    maxPrimeRange = 100
+    prime_number = generate_random_prime(minPrimeRange, maxPrimeRange)
+    print(prime_number)
+
