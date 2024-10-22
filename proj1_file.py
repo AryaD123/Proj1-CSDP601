19c5f409066bd4f82b30c43bcfe2e5a65815529c
Arya Das <aryad1@umbc.edu>
2024-10-22 18:34:44 -0400

    Continuing RSA algo functions

--- proj1_file.py
+++ proj1_file.py
@@ -34,22 +34,35 @@ def generate_random_prime(start, end):
         if is_prime(num):
             return num
 
+#function for setting each user's p & q values to random prime nums with the specified range
 def gen_rand_user_prime(user_dicts, minPrime, maxPrime):
     for i in user_dicts:
         i["p"] = generate_random_prime(minPrime, maxPrime)
         i["q"] = generate_random_prime(minPrime, maxPrime)
 
+def calc_n_phi(user_dicts):
+    for i in user_dicts:
+        n = i["p"] * i["q"]
+        i["n"] = n
+
+        phi = (i["p"] - 1) * (i["q"] - 1)
+        i["phi"] = phi
+
+
 if __name__ == '__main__':
     # Testing prime num generation
     minPrimeRange = 2
-    maxPrimeRange = 100
+    maxPrimeRange = 1000
     prime_number = generate_random_prime(minPrimeRange, maxPrimeRange)
-    print(prime_number)
+    print("Prime Num Test:", prime_number)
 
+    #storing users as dictionaries so process of storing/updating/adding values is simple and repeatable
     alice = {"p": -1, "q": -1}
     mike = {"p": -1, "q": -1}
     greg = {"p": -1, "q": -1}
     users = [alice, mike, greg]
+    #RSA Process Below, may Cosnolidate into single function as needed
     gen_rand_user_prime(users, minPrimeRange, maxPrimeRange)
+    calc_n_phi(users)
     print(users)
 
