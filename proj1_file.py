fe2a5a2f466e1c0e0c0746c5069fa70d2a741a7e
Arya Das <aryad1@umbc.edu>
2024-10-22 22:46:22 -0400

    Finished functions for getting input, converting into ascii, encrypting, and decrypting

--- proj1_file.py
+++ proj1_file.py
@@ -58,23 +58,61 @@ def rsaAlgoKeys(user_dicts, e_val, minPrime, maxPrime):
     calc_n_phi(user_dicts)
     compute_d(users, e_val)
 
-if __name__ == '__main__':
-    # Testing prime num generation
-    minPrimeRange = 2
-    maxPrimeRange = 1000
-    prime_number = generate_random_prime(minPrimeRange, maxPrimeRange)
-    print("Prime Num Test:", prime_number)
+def input_reciever(user_dicts, e_val):
+    print()
+    for i in range(len(user_dicts)):
+        print((i+1), ":", user_dicts[i]["Name"])
+    chosen = int(input("Choose a user or type -1 to exit: ")) - 1
+    if chosen < 0:
+        return -1
 
+    raw_msg = input("Enter a Message for user named " + user_dicts[chosen]["Name"] + ": ")
+    #user_dicts[chosen]["Input"] = raw_msg
+    raw_msg_split = []
+    ascii_msg = []
+    for j in raw_msg:
+        raw_msg_split.append(j)
+        ascii_msg.append(ord(j))
+    print(raw_msg_split)
+    print(ascii_msg)
+
+    encrypted_split_msg = []
+    for k in ascii_msg:
+        C = (k ** e_val) % user_dicts[chosen]["n"]
+        encrypted_split_msg.append(C)
+        #print(k, chr(k), "encrypted: ", C, chr(C))
+        print(f"Letter {chr(k)} (ascii {k}) ->encrypted-> {chr(C)} (Value {C})")
+
+    print()
+    decrypted_split_msg = []
+    for l in encrypted_split_msg:
+        M = (l ** user_dicts[chosen]["d"]) % user_dicts[chosen]["n"]
+        decrypted_split_msg.append(M)
+        #print(l, chr(l), "decrypted: ", M, chr(M))
+        print(f"Letter {chr(l)} (ascii {l}) ->decrypted-> {chr(M)} (Value {M})")
+
+    return 1
+
+
+if __name__ == '__main__':
+    print("Starting RSA Cryptography Algorithm...")
     #storing users as dictionaries so process of storing/updating/adding values is simple and repeatable
     alice = {"Name": "Alice", "p": -1, "q": -1}
     mike = {"Name": "Mike", "p": -1, "q": -1}
     greg = {"Name": "Greg", "p": -1, "q": -1}
     users = [alice, mike, greg]
-    #RSA Process Below, may consolidate into single function as needed
-    gen_rand_user_prime(users, minPrimeRange, maxPrimeRange)
-    calc_n_phi(users)
+
     standard_e_val = 65537 # picked because common public e values are 3, 5, 17, 257, or 65537
-    compute_d(users, standard_e_val)
+    minPrimeRange = 2
+    maxPrimeRange = 1000
+    rsaAlgoKeys(users, standard_e_val, minPrimeRange, maxPrimeRange)
 
-    print(users)
-
+    #print(users)
+    for i in users: print(i)
+    encrypt_decrypt_message = 0
+    while(encrypt_decrypt_message > -1):
+        print()
+        encrypt_decrypt_message = input_reciever(users, standard_e_val)
+        for i in users: print(i)
+        print()
+    print("...Finished Running")
